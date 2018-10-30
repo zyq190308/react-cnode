8b205e05d0802a75939867507075205d2baa679b
过客忆流年 <2938914315@qq.com>
2018-10-30 18:32:43 +0800

    write component by three ways

--- src/App.js
+++ src/App.js
@@ -3,18 +3,38 @@ import { connect } from 'react-redux';
 
 import { increment, decrement } from './action';
 
+//函数写无状态组件
+// const App = function(props) {
+//   return (
+//     <div className="App">
+//         <span>{ props.number }</span>
+//         <button onClick={() => props.increment(0)}>increment</button>
+//         <button onClick={() => props.decrement(1)}>decrement</button>
+//     </div>
+//   )
+// }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <span>{ this.props.number }</span>
-        <button onClick={() => this.props.increment(0)}>increment</button>
-        <button onClick={() => this.props.decrement(1)}>decrement</button>
-      </div>
-    );
-  }
-}
+//箭头函数写无状态组件
+const App = (props) => (
+  <div className="App">
+        <span>{ props.number }</span>
+        <button onClick={() => props.increment(0)}>increment</button>
+        <button onClick={() => props.decrement(1)}>decrement</button>
+  </div>
+)
+
+//继承方式写组件
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <span>{ this.props.number }</span>
+//         <button onClick={() => this.props.increment(0)}>increment</button>
+//         <button onClick={() => this.props.decrement(1)}>decrement</button>
+//       </div>
+//     );
+//   }
+// }
 
 const mapStateToProps = (state) => {
   return {
