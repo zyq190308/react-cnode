f42babead9e359fe9baa41b6864915dd3bb82399
过客忆流年 <2938914315@qq.com>
2018-10-30 14:40:55 +0800

    in(de)crement function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0
+    };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  increment() {
+    this.setState((state) => ({
+      number: ++state.number
+    }));
+  }
+  decrement() {
+    this.setState((state) => ({
+      number: --state.number
+    }));
+  }
   render() {
     return (
       <div className="App">
-        Hello React!
+        <span>{ this.state.number }</span>
+        <button onClick={this.increment}>increment</button>
+        <button onClick={this.decrement}>decrement</button>
       </div>
     );
   }
